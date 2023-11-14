ee118975758f42eafc338d58e362d36b8932c303
Md Adit Hasan <skadithasan19@gmail.com>
2023-11-14 11:14:51 -0600

    Initial Commit

---
+++ FetchiOS/FetchiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSApp.swift
+//  FetchiOS
+//
+//  Created by Adit Hasan on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
